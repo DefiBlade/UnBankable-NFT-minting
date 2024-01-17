8d49d886191d36a49d95f69d524f7c5375f0dc9d
DefiBlade <defiblade@gmail.com>
2024-01-17 00:07:46 +0000

    Add web3 integration

---
+++ src/providerOptions.js
@@ -0,0 +1,18 @@
+import WalletConnect from "@walletconnect/web3-provider";
+import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+
+export const providerOptions = {
+  walletlink: {
+    package: CoinbaseWalletSDK, // Required
+    options: {
+      appName: "Web 3 Modal Demo", // Required
+      infuraId: "9aa3d95b3bc440fa88ea12eaa4456161" // Required unless you provide a JSON RPC url; see `rpc` below
+    }
+  },
+  walletconnect: {
+    package: WalletConnect, // required
+    options: {
+      infuraId: "9aa3d95b3bc440fa88ea12eaa4456161" // required
+    }
+  }
+};
